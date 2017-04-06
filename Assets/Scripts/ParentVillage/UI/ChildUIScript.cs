b3547b18fdbddac9a790eab725412ce2b1176a62
Alan Wills <alawills@googlemail.com>
2017-04-06 20:44:25 +0100

    More money tweaks

--- Assets/Scripts/ParentVillage/UI/ChildUIScript.cs
+++ Assets/Scripts/ParentVillage/UI/ChildUIScript.cs
@@ -9,6 +9,8 @@ public class ChildUIScript : MonoBehaviour {
 
     public Child Child { get; set; }
 
+    private float timeHeld = 0;
+
     // Use this for initialization
     private void Awake()
     {
@@ -23,15 +25,35 @@ public class ChildUIScript : MonoBehaviour {
         ChildManager.ChildSelected += ChildManager_ChildSelected;
         animator = GetComponent<Animator>();
 	}
-    
+
+    public void Update()
+    {
+        if (Child.IsSelected)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                timeHeld += Time.deltaTime;
+                if (timeHeld > 0.5f)
+                {
+                    dataDialog.GetComponent<DataDialogScript>().Show(Child);
+                }
+            }
+            else
+            {
+                timeHeld = 0;
+                dataDialog.GetComponent<DataDialogScript>().Hide();
+            }
+        }
+    }   
+     
     public void Select()
     {
         ChildManager.SelectChild(Child);
-        //dataDialog.GetComponent<DataDialogScript>().Show(Child);
     }
 
     private void ChildManager_ChildSelected(Child child)
     {
+        timeHeld = 0;
         animator.SetBool("Animate", child == Child);
     }
 }
