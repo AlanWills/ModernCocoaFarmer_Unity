9a7363a605703f6c407bf253100d48144e174e0b
Alan Wills <alawills@googlemail.com>
2017-04-09 19:24:50 +0100

    Added labels to data dialog

---
+++ Assets/Scripts/ToolTipScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Collider2D))]
+public class ToolTipScript : MonoBehaviour {
+
+    public GameObject ToolTip;
+
+    private const float HoverTime = 0.25f;
+    private float currentTime = 0;
+    private Collider2D collider;
+
+	// Use this for initialization
+	void Start ()
+    {
+        collider = GetComponent<Collider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (collider.bounds.Contains(Input.mousePosition))
+        {
+            currentTime += Time.deltaTime;
+            if (currentTime >= HoverTime)
+            {
+                ToolTip.SetActive(true);
+            }
+        }
+        else
+        {
+            currentTime = 0;
+            ToolTip.SetActive(false);
+        }
+	}
+}
