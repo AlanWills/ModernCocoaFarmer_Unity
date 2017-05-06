e45150e1f6b8482a1adcb27848d9b62f53bedeac
Alan Wills <alawills@googlemail.com>
2017-05-06 19:15:04 +0100

    Updated animation controllers to use new macheteless sprite

---
+++ Assets/Scripts/PlayVideoScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayVideoScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        Handheld.PlayFullScreenMovie("IntroMovie.mp4");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
