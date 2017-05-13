f750191e5f0369baf7d9cd5ddff22cc0b898a93d
Alan Wills <alawills@googlemail.com>
2017-05-13 15:50:57 +0100

    Added pause button functionality

---
+++ Assets/Scripts/ParentVillage/UI/TimeStateButtonUIScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeStateButtonUIScript : MonoBehaviour
+{
+    public Sprite PlayingSprite;
+    public Sprite PausedSprite;
+
+    private Image imageUI;
+
+	// Use this for initialization
+	void Start ()
+    {
+        imageUI = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        imageUI.sprite = TimeManager.Paused ? PausedSprite : PlayingSprite;
+    }
+
+    public void ToggleTimeState()
+    {
+        TimeManager.Paused = !TimeManager.Paused;
+    }
+}
