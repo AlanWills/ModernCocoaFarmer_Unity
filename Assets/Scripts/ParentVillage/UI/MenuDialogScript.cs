a8301bf4f9b8d70a6b12cf47f26682a52d92a559
Alan Wills <alawills@googlemail.com>
2017-05-14 17:52:19 +0100

    Added turning off of audio

--- Assets/Scripts/ParentVillage/UI/MenuDialogScript.cs
+++ Assets/Scripts/ParentVillage/UI/MenuDialogScript.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuDialogScript : MonoBehaviour
 {
+    public Sprite AudioPlaying;
+    public Sprite AudioMuted;
+
     private GameObject menuDialogUI;
+    private Button audioButton;
 
 	// Use this for initialization
 	void Start ()
     {
         menuDialogUI = GameObject.Find("MenuDialogUI");
         menuDialogUI.SetActive(false);
+        audioButton = menuDialogUI.transform.FindChild("Buttons").FindChild("AudioButton").GetComponent<Button>();
 	}
 	
 	// Update is called once per frame
@@ -29,5 +35,11 @@ public class MenuDialogScript : MonoBehaviour
     {
         TimeManager.Paused = false;
         menuDialogUI.SetActive(false);
+    }
+
+    public void ToggleAudio()
+    {
+        AudioListener.pause = !AudioListener.pause;
+        audioButton.image.sprite = AudioListener.pause ? AudioMuted : AudioPlaying;
     }
 }
