66dd367394a38c217b135899bdf7957d463897f9
Alan Wills <alawills@googlemail.com>
2017-05-15 20:16:47 +0100

    Updated some assets

--- Assets/Scripts/PlayVideoScript.cs
+++ Assets/Scripts/PlayVideoScript.cs
@@ -8,7 +8,7 @@ public class PlayVideoScript : MonoBehaviour
     public string SceneName;
 
 	// Use this for initialization
-	void Awake ()
+	void Start()
     {
         Handheld.PlayFullScreenMovie("IntroMovie.mp4", Color.black, FullScreenMovieControlMode.CancelOnInput);
         SceneManager.LoadSceneAsync(SceneName);
