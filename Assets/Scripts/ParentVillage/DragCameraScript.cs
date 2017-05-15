35bafc195ad5a453adbcefd5c090a559a38e1d8e
Alan Wills <alawills@googlemail.com>
2017-05-15 21:16:14 +0100

    Trying to fix camera drag script

--- Assets/Scripts/ParentVillage/DragCameraScript.cs
+++ Assets/Scripts/ParentVillage/DragCameraScript.cs
@@ -6,10 +6,17 @@ public class DragCameraScript : MonoBehaviour {
 
     bool mouseDownLastFrame = false;
     private Vector3 lastPosition;
+    private float maxXTranslation = 0;
+    private float maxYTranslation = 0;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        Vector3 totalSize = GameObject.Find("VillageBackground").GetComponent<SpriteRenderer>().bounds.extents * 2;
+        Vector3 screenDimensionsInWorldSpace = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0)) * 2;
+
+        maxXTranslation = (totalSize.x - screenDimensionsInWorldSpace.x) * 0.5f;
+        maxYTranslation = (totalSize.y - screenDimensionsInWorldSpace.y) * 0.5f;
 	}
 	
 	// Update is called once per frame
@@ -23,7 +30,10 @@ public class DragCameraScript : MonoBehaviour {
             }
             else
             {
-                transform.localPosition -= (Input.mousePosition - lastPosition) * 0.01f;
+                Vector3 newPosition = transform.localPosition - (Input.mousePosition - lastPosition) * 0.01f;
+                newPosition.x = Mathf.Clamp(newPosition.x, -maxXTranslation, maxXTranslation);
+                newPosition.y = Mathf.Clamp(newPosition.y, -maxYTranslation, maxYTranslation);
+                transform.localPosition = newPosition;
             }
 
             lastPosition = Input.mousePosition;
